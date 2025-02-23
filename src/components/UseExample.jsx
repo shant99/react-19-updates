228f1349890af568733fcc4b44dd1cbcb5495ead
shant99 <shant.hakobyan.999@gmail.com>
2025-02-23 14:28:22 +0400

    use example

---
+++ src/components/UseExample.jsx
@@ -0,0 +1,19 @@
+import { use } from "react";
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (r) => r.json()
+);
+
+export default function UseExample() {
+  const users = use(fetchUsers);
+
+  return (
+    <ul className="collection">
+      {users.map((user) => (
+        <li key={user.id} className="collection-item">
+          {user.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
